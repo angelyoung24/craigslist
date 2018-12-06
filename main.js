44946d015071b9cfcd19becdc065df3366e4b7ac
Angel <angel.m.young24@gmail.com>
2018-12-06 01:29:21 -0500

    second commit

--- main.js
+++ main.js
@@ -1,32 +1,16 @@
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
-var input, filter, a, i;
-input = document.getElementById("myStates");
-div = document.getElementById("thisState");
-filter = input.value.toUpperCase();
-a = div.getElementsByTagName("a");
 
-function dropStates() {
-    document.getElementById("myStates").classList.toggle("show");
+function dropProv() {
+    document.getElementById("myProv").classList.toggle("show");
 }
 
-
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function (event) {
-    if (!event.target.matches('.dropbtn')) {
-
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
-    }
-}
-
-function stateSearch() {
+function searchProv() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("thisProv");
+    filter = input.value.toUpperCase();
+    div = document.getElementById("myProv");
+    a = div.getElementsByTagName("a");
     for (i = 0; i < a.length; i++) {
         if (a[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
             a[i].style.display = "";
@@ -35,6 +19,30 @@ function stateSearch() {
         }
     }
 }
+
+
+
+
+function dropStates() {
+    document.getElementById("myStates").classList.toggle("show");
+}
+
+function stateSearch() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("thisState");
+    filter = input.value.toUpperCase();
+    div = document.getElementById("myStates");
+    a = div.getElementsByTagName("a");
+    for (i = 0; i < a.length; i++) {
+        if (a[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
+            a[i].style.display = "";
+        } else {
+            a[i].style.display = "none";
+        }
+    }
+}
+
+
 
 
 
